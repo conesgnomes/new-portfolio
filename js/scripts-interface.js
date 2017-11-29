ef3c915b4ef565fd414e6a4a8e92430e5e261e83
conesgnomes <connorlarsen@gmail.com>
2017-11-28 17:45:24 -0800

    finish styling nav bar on all pages

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -3,40 +3,48 @@ $(window).scroll(function(){
   if ($(window).width() <= 759 && $('body').hasClass("index")) {
     if ($(window).scrollTop() >= 60) {
       $('.navigation').addClass('fixed-mobile');
+      $('.title').fadeOut(90);
     }
     else {
       $('.navigation').removeClass('fixed-mobile');
+      $('.title').fadeIn(90);
     }
   }
   if ($(window).width() >= 760 && $(window).width() <= 999 && $('body').hasClass("index")) {
     if ($(window).scrollTop() >= 20) {
       $('.navigation').addClass('fixed-tablet');
+      $('.title').fadeOut(90);
     }
     else {
       $('.navigation').removeClass('fixed-tablet');
+      $('.title').fadeIn(90);
     }
   }
-  if ($(window).width() >= 1000 && $(window).width() <= 1199 && $('body').hasClass("index")) {
+  if ($(window).width() >= 900 && $(window).width() <= 1199 && $('body').hasClass("index")) {
     if ($(window).scrollTop() >= 40) {
       $('.navigation').addClass('fixed-desktop');
+      $('.title').fadeOut(90);
     }
     else {
       $('.navigation').removeClass('fixed-desktop');
+      $('.title').fadeIn(90);
     }
   }
   if ($(window).width() >= 1200 && $('body').hasClass("index")) {
     if ($(window).scrollTop() >= 42) {
       $('.navigation').addClass('fixed-desktop');
+      $('.title').fadeOut(90);
     }
     else {
       $('.navigation').removeClass('fixed-desktop');
+      $('.title').fadeIn(90);
     }
   }
 
   // Fixed header for all subpages
 
   if ($(window).width() <= 759 && $('body').hasClass("subpage")) {
-    if ($(window).scrollTop() >= 60) {
+    if ($(window).scrollTop() >= 30) {
       $('.navigation').addClass('fixed-mobile-sub');
     }
     else {
